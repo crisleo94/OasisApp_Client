6673478a34339674d47441b0df309390ee8bd586
crisleo94 <cristhian.reinoso@live.com>
2019-07-10 15:54:25 -0500

    Servicio para los mapas de google, servicio para la autenticación con google, registro de nuevos usuarios y login de usuarios registrados con firebase

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,4 @@
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { UsuarioModel } from './../../models/usuario.model';
 import { Router } from '@angular/router';
 import { MenuController, AlertController } from '@ionic/angular';
@@ -13,22 +13,72 @@ export class RegisterPage implements OnInit {
 
   habilitado = false;
   user = new UsuarioModel();
+  registro: FormGroup;
+  errorMessage = '';
+  successMessage = '';
 
-  constructor(private menu: MenuController, private router: Router, private alert: AlertController) { }
+  validationMessages = {
+    cssClass: 'errorPass',
+    email: [
+      {type: 'required', message: '*El correo es obligatorio'},
+      {type: 'pattern', message: '*Ingrese un correo válido'}
+    ],
+    password: [
+      {type: 'required', message: '*La contraseña es obligatoria'},
+      {type: 'minLength', message: '*La contraseña debe tener mínimo 6 caracteres'}
+    ],
+    nombre: [
+      {type: 'required', message: '*El nombre es obligatorio'},
+      {type: 'minLength', message: '*Este campo debe tener mínimo 5 caracteres'}
+    ],
+    password2: [
+      {type: 'required', message: '*Las contraseñas deben coincidir'},
+      {type: 'minLength', message: '*La contraseña debe tener mínimo 6 caracteres'}
+    ],
+    usuario: [
+      {type: 'required', message: '*El nombre de usuario es requerido'},
+      {type: 'minLength', message: '*El nombre de usuario debe tener mínimo 6 caracteres'}
+    ]
+  };
+
+  constructor(private menu: MenuController,
+              private router: Router,
+              private alert: AlertController,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
     this.menu.enable(this.habilitado);
+    this.registro = this.fb.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ])),
+      usuario: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ])),
+      nombre: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])),
+      password2: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(6)
+      ]))
+    });
   }
 
-  register(form: NgForm) {
+  register(form) {
     if (form.valid) {
-      this.router.navigateByUrl('/register2');
-    } else {
-      this.errorRegister();
-    }
+      console.log(form);
+    } else { console.log(form); }
   }
 
-  async errorRegister() {
+  /* async errorRegister() {
     const alert = await this.alert.create({
       header: 'Error',
       message: 'Debe llenar todos los campos para continuar',
@@ -36,7 +86,7 @@ export class RegisterPage implements OnInit {
     });
 
     await alert.present();
-  }
+  } */
 
 
 }
--- src/app/pages/register2/register2.page.ts
+++ src/app/pages/register2/register2.page.ts
@@ -1,3 +1,14 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps/ngx';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from './../../models/usuario.model';
@@ -13,11 +24,19 @@ export class Register2Page implements OnInit {
 
   user = new UsuarioModel();
   habilitado = false;
+  longitud;
+  latitud;
+  map: GoogleMap;
+  // map2: GoogleMap;
 
-  constructor(private menu: MenuController, private router: Router, private alert: AlertController) { }
+  constructor(private menu: MenuController,
+              private router: Router,
+              private alert: AlertController,
+              private locate: Geolocation) { }
 
   ngOnInit() {
     this.menu.enable(this.habilitado);
+    this.cargarMapa();
   }
 
   register2(form: NgForm) {
@@ -36,6 +55,51 @@ export class Register2Page implements OnInit {
     });
 
     await alert.present();
+  }
+
+  getPosition() {
+    // Con esta función se obtienen las coordenadas y se envian a cargarMapa
+    this.locate.getCurrentPosition().then((resp) => {
+      this.latitud = resp.coords.latitude;
+      this.longitud = resp.coords.longitude;
+    }).catch((error) => {
+      // En caso se registre un error
+      console.error('Error obteniendo la ubicación', error);
+    });
+  }
+
+  cargarMapa() {
+    // Se pasan las coordenadas obtenidas con el getposition
+    console.log(this.latitud, this.longitud);
+
+    Environment.setEnv({
+      // API_KEY_FOR_BROWSER_RELEASE: '(https://maps.googleapis.com/maps/api/js?key=AIzaSyB9MVUJdvr1kNfn5goUrxxN3lRbvMVq40I)',
+      API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyCt7WPEfZCJFecvCoQb24L3IJejmFznsZk'
+    });
+
+    // Crear el mapa
+    const mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: this.latitud,
+          lng: this.longitud
+        },
+        zoom: 12,
+        tilt: 30
+      }
+    };
+    this.map = GoogleMaps.create('mapa', mapOptions);
+
+    const marker: Marker = this.map.addMarkerSync({
+      title: 'Addres Location',
+      icon: 'Red',
+      animation: 'DROP',
+      position: {
+        lat: this.latitud,
+        lng: this.longitud
+      }
+    });
+    marker.showInfoWindow();
   }
 
 }
